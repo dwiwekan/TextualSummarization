0e03f94e5e9c269d0fa26680f28e293cede1468b
Ida bagus Dwiweka Naratama <dwiwekanaratama@student.unud.ac.id>
2023-03-03 22:03:48 +0800

    Venv

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, jsonify, request
 from TextualSummarization import TextualSummarization
 
-data = ''
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/name", methods=["POST"])
+@app.route("/help")
+def help():
+    return "<p>Help</p>"
+
+@app.route("/predict", methods=["POST"])
 def setName():
     if request.method=='POST':
         posted_data = request.get_json()
@@ -18,13 +21,4 @@ def setName():
         posisi = posted_data['posisi']
         ts = TextualSummarization()
         summary = ts.predict(text, persentase, awal_akhir, posisi)
-        
         return jsonify(summary)
-
-    
-@app.route("/message", methods=["GET"])
-def message():
-    posted_data = request.get_json()
-    data 
-    name = posted_data['name']
-    return jsonify(" Hope you are having a good time " +  name + "!!!")
